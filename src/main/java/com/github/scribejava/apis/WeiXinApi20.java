52fd2cdf47964c4c3ffafb44ee3937b7e4aeb090
hnxyhcwdl1003@163.com <hnxyhcwdl1003@163.com>
2018-02-09 18:39:42 +0800

    test

--- src/main/java/com/github/scribejava/apis/WeiXinApi20.java
+++ src/main/java/com/github/scribejava/apis/WeiXinApi20.java
@@ -5,17 +5,13 @@ package com.github.scribejava.apis;
 
 import com.github.scribejava.apis.service.ExtOAuth20Service;
 import com.github.scribejava.core.builder.api.DefaultApi20;
-import com.github.scribejava.core.extractors.OAuth2AccessTokenExtractor;
-import com.github.scribejava.core.extractors.TokenExtractor;
-import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.github.scribejava.core.model.OAuthConfig;
-import com.github.scribejava.core.model.Verb;
 import com.github.scribejava.core.oauth.OAuth20Service;
 
 public class WeiXinApi20 extends DefaultApi20 {
 
-	public static final String AUTHORIZE_URL = "https://open.weixin.qq.com/connect/qrconnect";
-	public static final String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/sns/oauth2/access_token";
+	private static final String AUTHORIZE_URL = "https://open.weixin.qq.com/connect/qrconnect";
+	private static final String ACCESS_TOKEN_URL = "https://api.weixin.qq.com/sns/oauth2/access_token";
 
 	protected WeiXinApi20() {
 	}
@@ -29,25 +25,15 @@ public class WeiXinApi20 extends DefaultApi20 {
 	}
 
 	@Override
-	public Verb getAccessTokenVerb() {
-		return Verb.GET;
-	}
-
-	@Override
 	public String getAccessTokenEndpoint() {
 		return ACCESS_TOKEN_URL;
 	}
-
+	
 	@Override
 	protected String getAuthorizationBaseUrl() {
 		return AUTHORIZE_URL;
 	}
-
-	@Override
-	public TokenExtractor<OAuth2AccessToken> getAccessTokenExtractor() {
-		return OAuth2AccessTokenExtractor.instance();
-	}
-
+	
 	@Override
 	public OAuth20Service createService(OAuthConfig config) {
 		return new ExtOAuth20Service(this, config);
