94e6bf95f20d37d6c6f2a73f1de32e436c31e270
vindell <hnxyhcwdl1003@163.com>
2018-08-28 22:04:01 +0800

    修改：更新依赖版本

--- src/main/java/com/github/scribejava/apis/service/ExtOAuth20Service.java
+++ src/main/java/com/github/scribejava/apis/service/ExtOAuth20Service.java
@@ -4,17 +4,20 @@
 package com.github.scribejava.apis.service;
 
 import com.github.scribejava.core.builder.api.DefaultApi20;
-import com.github.scribejava.core.model.OAuthConfig;
+import com.github.scribejava.core.httpclient.HttpClient;
+import com.github.scribejava.core.httpclient.HttpClientConfig;
 import com.github.scribejava.core.model.OAuthRequest;
 import com.github.scribejava.core.oauth.OAuth20Service;
 
 public class ExtOAuth20Service extends OAuth20Service {
 
-    public ExtOAuth20Service(DefaultApi20 api, OAuthConfig config) {
-        super(api, config);
-    }
-    
-    @Override
+	public ExtOAuth20Service(DefaultApi20 api, String apiKey, String apiSecret, String callback, String scope,
+			String state, String responseType, String userAgent, HttpClientConfig httpClientConfig,
+			HttpClient httpClient) {
+		super(api, apiKey, apiSecret, callback, scope, state, responseType, userAgent, httpClientConfig, httpClient);
+	}
+
+	@Override
     protected OAuthRequest createAccessTokenRequest(String code, String pkceCodeVerifier) {
     	OAuthRequest request = super.createAccessTokenRequest(code, pkceCodeVerifier);
     	request.addParameter("dataType", "json");
--- src/main/java/com/github/scribejava/apis/service/OschinaOAuth20ServiceImpl.java
+++ src/main/java/com/github/scribejava/apis/service/OschinaOAuth20ServiceImpl.java
@@ -1,7 +1,8 @@
 package com.github.scribejava.apis.service;
 
 import com.github.scribejava.core.builder.api.DefaultApi20;
-import com.github.scribejava.core.model.OAuthConfig;
+import com.github.scribejava.core.httpclient.HttpClient;
+import com.github.scribejava.core.httpclient.HttpClientConfig;
 import com.github.scribejava.core.model.OAuthRequest;
 import com.github.scribejava.core.oauth.OAuth20Service;
 
@@ -9,10 +10,12 @@ import com.github.scribejava.core.oauth.OAuth20Service;
  */
 public class OschinaOAuth20ServiceImpl extends OAuth20Service {
 
-    public OschinaOAuth20ServiceImpl(DefaultApi20 api, OAuthConfig config) {
-        super(api, config);
-    }
-
+	public OschinaOAuth20ServiceImpl(DefaultApi20 api, String apiKey, String apiSecret, String callback, String scope,
+			String state, String responseType, String userAgent, HttpClientConfig httpClientConfig,
+			HttpClient httpClient) {
+		super(api, apiKey, apiSecret, callback, scope, state, responseType, userAgent, httpClientConfig, httpClient);
+	}
+	
     @Override
     protected OAuthRequest createAccessTokenRequest(String code, String pkceCodeVerifier) {
     	OAuthRequest request = super.createAccessTokenRequest(code, pkceCodeVerifier);
